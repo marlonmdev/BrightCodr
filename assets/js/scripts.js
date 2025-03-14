045d06919094c4704e3f05a3b8b480932a54bfd8
Marlon H. Muring <114371971+marlonmdev@users.noreply.github.com>
2025-03-14 19:19:31 +0800

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const navLinks = document.querySelectorAll('.nav-link');
+
+  navLinks.forEach(link => {
+      link.addEventListener('click', function () {
+          // Remove 'active' class from all links
+          navLinks.forEach(link => link.classList.remove('active'));
+          // Add 'active' class to the clicked link
+          this.classList.add('active');
+      });
+  });
+});
